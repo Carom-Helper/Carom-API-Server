940013b1c5ecade05254bb6034ec29df95b2bb83
taktakgyun <vbmrkk@naver.com>
2022-11-15 17:22:35 +0900

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,6 @@
+CUR_PATH = "`pwd`"
 cd detection/detect/npu_yolov5/utils/box_decode/cbox_decode
-rm -rf build cbox_decode.so
+# rm -rf build cbox_decode.so
+mv cbox_decode.so ../
 python setup.py build_ext --inplace
-cd -
+cd CUR_PATH
