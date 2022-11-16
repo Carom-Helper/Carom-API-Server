e726e30395ccc70f8007257f4f9806c702f1970d
taktakgyun <vbmrkk@naver.com>
2022-11-16 19:24:19 +0900

    update

--- src/recommend/route/WallObject.py
+++ src/recommend/route/WallObject.py
@@ -1,6 +1,5 @@
-from action_cls import (
-    IObserver, ISubject, IMoveable, ICrashable, is_test
-)
+from action_cls import *
+    # IObserver, ISubject, IMoveable, ICrashable, is_test)
 
 def is_test_wallobject()->bool:
     return False and is_test()
@@ -10,4 +9,7 @@ def test_print(s, s1="", s2="", s3="", s4="", s5="", end="\n"):
         print("wall object exe : ", s, s1, s2, s3, s4, s5, end=end)
         
         
-class WallObject()
+class WallObject(IMovableObserver):
+    def __init__(self) -> None:
+        super().__init__()
+        self.subject = CrashSubject
