bb9341bdf75af6b66fb3e89f3d4011412936d9d8
qjrm69 <qjrm69@gmail.com>
2022-11-17 00:37:51 +0900

    Update action_cls.py

--- src/recommend/route/action_cls.py
+++ src/recommend/route/action_cls.py
@@ -115,62 +115,35 @@ class CaromBall(IMoveable, ICrashable, IObserver, ISubject):
         self.power = power
         self.theta = clock % 12 * (-30) + 90
         self.tip = tip
+
         upspinmax = 3  * math.sin(math.pi * (90 / 180)) * 50 * radius
         upspinmin = 3  * math.sin(math.pi * (-60 / 180)) * 50 * radius
         self.upspin = math.sin(math.pi * (self.theta/180)) * tip * self.power * radius
-        upspinrate = int((self.upspin - upspinmin) / (upspinmax-upspinmin) * 10)
-
-        self.upspinrate = upspinrate
+        self.upspinrate = int((self.upspin - upspinmin) / (upspinmax-upspinmin) * 10)
 
         sidespinmax = 3 * math.cos(math.pi * (0 / 180)) * 50 * radius
         sidespinmin = 3 * math.cos(math.pi * (-180 / 180)) * 50 * radius
         self.sidespin = math.cos(math.pi * (self.theta/180)) * tip * self.power * radius
-        sidespinrate = int((self.sidespin - sidespinmin) / (sidespinmax-sidespinmin) * 10)
+        self.sidespinrate = int((self.sidespin - sidespinmin) / (sidespinmax-sidespinmin) * 10)
 
-        self.sidespinrate = sidespinrate-5
-    
     def print_param(self):
         print(f'theta: {self.theta}, tip: {self.tip}/3')
         print(f'upspin: {self.upspin:0.2f}, sidespin: {self.sidespin:0.2f}')
         print(f'upspinrate: {self.upspinrate}, sidespinrate: {self.sidespinrate}\n')
     
-    @abstractclassmethod
-    def get_range(self, data=dict())->float:
+    def update(self, event:dict=None) -> None:
         pass
-    @abstractclassmethod
-    def get_xy(self)->list:
-        pass
-    
-    
-class CrashableSubject(ICrashable, ISubject, meta=ABCMeta):
-    def __init__(self) -> None:
-        super().__init__()
-        self.crash_event = None
-    
-    def set_crash_event(self, crash_action:ICrashAction):
-        self.crash_event = crash_action
-    
-    @abstractclassmethod
+
     def notify_observers(self):
         pass
-        # # observer들을 하나씩 방문하면서
-        # for idx, observer in enumerate(self.observer_list):
-        #     # 충돌 인지를 판정한다.
-        #     if self.check_crash(idx):
-        #         # 충돌 했다면, 충돌을 전파한다.
-        #         observer.crash(self)
-        #         # 그리고 자기도 충돌 행동을 한다. 하지만 안한다.
-    # x,y 점과 자신과의 거리를 반환한다.
-    @abstractclassmethod
-    def get_distance_from_point(self, x:float, y:float)-> float:
+
+    def get_distance_from_point(x:float, y:float)-> float:
         pass
-    # 점에 대한 법선 벡트를 반환한다.
-    @abstractclassmethod
+
     def get_normal_vector(self, x:float, y:float)-> np.array:
         pass
-    # 점에 대한 법선 벡트를 반환한다.
-    @abstractclassmethod
-    def get_reflect_closure(self):
+    
+    def crash(self, normal_vec:np.array):
         pass
 
     def get_xy(self)->list:
@@ -196,17 +169,6 @@ class CrashableSubject(ICrashable, ISubject, meta=ABCMeta):
         xy = {"x": new_x, "y": new_y, "t": self.xy[-1]["elapsed"] + elapsed}
 
         return xy
-
-class IMovableObserver(IMoveable, IObserver):
-    def __init__(self) -> None:
-        super().__init__()
-    @abstractclassmethod
-    def get_xy(self)->list:
-        pass
-    @abstractclassmethod
-    def update(self, event:dict=None) -> None:
-        pass
-
 
 def set_vec(cue:CaromBall, tar:CaromBall, thickness:float)->dict:
     radius = 8.6
@@ -238,6 +200,7 @@ def set_vec(cue:CaromBall, tar:CaromBall, thickness:float)->dict:
     
     cue.vector["x"] = vector["x"]
     cue.vector["y"] = vector["y"]
+        
 
 class ICrashObserver(ICrash, IObserver, meta=ABCMeta):
     def __init__(self) -> None:
@@ -278,10 +241,11 @@ class MoveableSubject(IMoveable, ISubject, meta=ABCMeta):
         if observer is not ICrashObserver: raise TypeError
         return super().register_observer(observer)
     
-    def check_crash(self, observer:ICrash):
+    def check_crash(self, observer:ICrash)-> bool:
+        if observer is not ICrash: raise TypeError()
         x,y = self.get_xy()
         distance = observer.get_distance_from_point(x,y)
-        return (distance - self.get_range()) < self.elapse
+        return ((distance - self.get_range()) < self.elapse)
     
     # 자신과 충돌한 것이 있는지 없는지 확인한다. 
     # 그리고 충돌한 것이 있다면, 발견한 즉시 검색을 멈추고 충돌을 전파한다.
